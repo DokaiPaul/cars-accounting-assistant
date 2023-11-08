8221a35e1bd3ade83fda67c4737c0594806aecf0
dokay <dokaypaul@gmail.com>
2023-11-08 21:37:38 +0200

    feat: add User entity and basic operations with users via admin use cases

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './features/users/users.module';
 import { POSTGRESQL_CONFIGURATION } from './db/postgre-sql/connection.configuration';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './features/users/domain/entities/user.entity';
 
 @Module({
   imports: [UsersModule, POSTGRESQL_CONFIGURATION],
